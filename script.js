115d236a377ccf8d03d2eb37a24d2ef68bb1266f
James M Thankachen <jamesmanjada@gmail.com>
2023-08-13 00:55:04 +0530

    JS update

--- script.js
+++ script.js
@@ -6,13 +6,3 @@ menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   showcase.classList.toggle("active");
 });
-
-// const videoElement = document.getElementById("bg-video");
-// videoElement.addEventListener("loadeddata", (e) => {
-//   console.log(e);
-//   //Video should now be loaded but we can add a second check
-//   if (videoElement.readyState === 4) {
-//     console.log(videoElement.readyState)
-//     //your code goes here
-//   }
-// });
