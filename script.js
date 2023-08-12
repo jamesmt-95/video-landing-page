8913d5d0a6e144c3240555a1eb0c3ef03a99a6f9
James M Thankachen <jamesmanjada@gmail.com>
2023-08-13 00:31:02 +0530

    Video Update

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const menuToggle = document.querySelector('.toggle');
-const showcase = document.querySelector('.showcase');
-const menu = document.querySelector('.menu');
+const menuToggle = document.querySelector(".toggle");
+const showcase = document.querySelector(".showcase");
+const menu = document.querySelector(".menu");
 
-menuToggle.addEventListener('click', ()=>{
-    menuToggle.classList.toggle('active');
-    showcase.classList.toggle('active');
-})
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  showcase.classList.toggle("active");
+});
+
+// const videoElement = document.getElementById("bg-video");
+// videoElement.addEventListener("loadeddata", (e) => {
+//   console.log(e);
+//   //Video should now be loaded but we can add a second check
+//   if (videoElement.readyState === 4) {
+//     console.log(videoElement.readyState)
+//     //your code goes here
+//   }
+// });
